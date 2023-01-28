58bfef91036e852b38d60edf0433a3690c33ae9e
Shagun Mengi <shagunmengi003@gmail.com>
2023-01-28 16:03:02 +0530

    todoapp

---
+++ MERN-Todo-App-main/server/models/todoItems.js
@@ -0,0 +1,15 @@
+//import mongoose to create mongoose model
+const mongoose = require('mongoose');
+
+//create Schema
+const TodoItemSchema = new mongoose.Schema({
+  item:{
+    "type":"String",
+    "required": "true",
+    "minLength": 3,
+    "maxLength": 7
+  }
+})
+
+//export this Schema
+module.exports = mongoose.model('todo', TodoItemSchema);
